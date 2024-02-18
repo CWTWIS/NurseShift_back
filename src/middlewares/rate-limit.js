a088cd35bdc3dc3d7b8852f98f2f124f59fbfffe
CWTWIS <chawanrat.wis@gmail.com>
2024-02-19 00:35:44 +0700

    able to edit

--- src/middlewares/rate-limit.js
+++ src/middlewares/rate-limit.js
@@ -2,6 +2,6 @@ const { rateLimit } = require("express-rate-limit");
 
 module.exports = rateLimit({
   windowMs: 1000 * 60 * 5,
-  limit: 1000,
+  limit: 1000000000,
   message: { message: "too many request in a given period" },
 });
